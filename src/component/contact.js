364c1f8aaa56336f7aac9a8a19b0a7bebf24d3a2
Alan <acarrillo4@mail.csuchico.edu>
2023-02-05 19:52:47 -0800

    contacts page

--- src/component/contact.js
+++ src/component/contact.js
@@ -1,9 +1,23 @@
+import { MDBContainer, MDBInput } from "mdb-react-ui-kit";
 import "./../styles.css";
+import React, { useState } from "react";
 
 export default function Contact(){
+    const [name, setName]=useState([]);
+
     return(
     <>
         <h1>Contacts</h1>
+        <MDBContainer className="bg-dark">
+            <h2>Get In Touch</h2>
+            <form>
+                <MDBInput label="Name"/>
+                <MDBInput label="Email address"/>
+                <MDBInput label="Message"/>
+            </form>
+
+
+        </MDBContainer>
     </>
     );
 }
