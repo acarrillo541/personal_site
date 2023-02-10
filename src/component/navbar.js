a11654220ce0f4e28f410ee9127ac005f90815b5
Alan <acarrillo4@mail.csuchico.edu>
2023-02-09 17:03:11 -0800

    update navbar

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     MDBNavbar,
-    MDBCol,
-    MDBNavbarLink,
     MDBIcon,
     MDBBtn,
+    MDBTabs,
+    MDBTabsItem,
+    MDBTabsLink,
 } from 'mdb-react-ui-kit';
 
 const Btn = ({type, color, link}) =>{
@@ -15,20 +16,42 @@ const Btn = ({type, color, link}) =>{
     );
 }
 
+
+
 const Navbar=()=>{
+    const [tab, setTab] = useState('Projects');
+
+    const TabItem = ({label})=>{
+        return(
+            <MDBTabsItem>
+                <MDBTabsLink className='text-white' style={{backgroundColor:'transparent'}} onClick={()=>{setTab(label);}} active={tab===label}>
+                    {label}
+                </MDBTabsLink>
+            </MDBTabsItem>
+        );
+    }
+
+
     return(
         <>
-            <MDBNavbar  style={{backgroundColor:'transparent'}}>
-               <h2 style={{whiteSpace:'pre-line'}} className="fw-bolder text-white ms-4 mt-3 lh-1">
+            <MDBNavbar style={{backgroundColor:'transparent'}}>
+                 <h2 style={{whiteSpace:'pre-line'}} 
+                    className="fw-bolder text-white ms-4 mt-3 lh-1">
                     Alan{"\n"}Carrillo
                 </h2>
-                    <MDBNavbarLink>Experience</MDBNavbarLink>
-                    <MDBNavbarLink>Contact Me</MDBNavbarLink>
+
+                <div className='d-flex justify-content-end me-4' >
+                    <MDBTabs>
+                        <TabItem label={'Home'}/>
+                        <TabItem label={'Projects'}/>
+                        <TabItem label={'Contact'}/>
+                    </MDBTabs>
               
-                <MDBCol className='d-flex justify-content-end align-items-center' >
-                    <Btn type="github" color="#333333" link="https://www.github.com/acarrillo541"/>
-                    <Btn type="linkedin" color="#0082ca" link="http://www.linkedin.com"/>
-                </MDBCol>
+                    <div className='ms-4'>
+                        <Btn type="github" color="#333333" link="https://www.github.com/acarrillo541"/>
+                        <Btn type="linkedin" color="#0082ca" link="http://www.linkedin.com"/>
+                    </div>
+                </div>
             </MDBNavbar>
         </>
     );
