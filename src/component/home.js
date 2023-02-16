ec38b1f46b6a381a2b218a0b03844ee67c829528
Alan <acarrillo4@mail.csuchico.edu>
2023-02-15 22:40:13 -0800

    reword contact, get email working next

--- src/component/home.js
+++ src/component/home.js
@@ -3,7 +3,8 @@ import "./../styles.css";
 export default function Home(){
     return(
     <>
-        <div className="homePage">
+    <div className="homePage">
+        <div className="rounded-9 p-5" style={{backgroundColor:"#DEF2F1"}}>
             <p className="home-body">Hello, my name is</p>
             <h1 className="bigHeader" style={{color:'#3AAFA9'}}>Alan Carrillo.</h1>
             <h1 className="bigHeader" style={{color:'#2B7A78'}}>I build web and mobile apps.</h1>
@@ -11,8 +12,8 @@ export default function Home(){
                 I am a software developer based out of the San Francisco Bay Area
                 with a passion for making ideas come to life.
             </p>
-
         </div>
+    </div>
     </>
     );
 }
